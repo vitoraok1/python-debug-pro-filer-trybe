5fa39748794d36c841b61cb81be2c923445ef7d5
Vitor Aoki <vitoraoki17@gmail.com>
2023-08-16 15:25:37 -0300

    requirement 01 - debugging show_deepest_file function

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -1,11 +1,17 @@
 """Arquivo que estudantes devem editar"""
 
+"""Utilização da função lambda para criar uma função anônima que recebe vários
+argumentos: https://pythonacademy.com.br/blog/funcoes-lambda-no-python"""
+
 
 def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(
+            context["all_files"],
+            key=lambda path: len(path.split("/")),
+        )
         print(f"Deepest file: {deepest_file}")
 
 
